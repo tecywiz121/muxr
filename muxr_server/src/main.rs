4d5dd12d20dc22395f6165953d6803b1d0bf910d
Sam Wilson <tecywiz121@hotmail.com>
2019-03-26 16:53:31 +0000

    Use quick_main from error-chain

--- muxr_server/src/main.rs
+++ muxr_server/src/main.rs
@@ -31,17 +31,7 @@ use tokio::prelude::*;
 
 use tokio_codec::Framed;
 
-fn main() {
-    if let Err(ref e) = run() {
-        use error_chain::ChainedError;
-        use std::io::Write; // trait which holds `display_chain`
-        let stderr = &mut ::std::io::stderr();
-        let errmsg = "Error writing to stderr";
-
-        writeln!(stderr, "{}", e.display_chain()).expect(errmsg);
-        ::std::process::exit(1);
-    }
-}
+quick_main!(run);
 
 fn run() -> Result<()> {
     let stdout = File::create("/tmp/daemon.out").unwrap();
